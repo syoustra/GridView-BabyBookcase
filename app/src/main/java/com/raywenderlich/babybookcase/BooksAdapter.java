432cfb20aa848771833ba1806f464b76ac9e34c5
Stephanie Youstra <steph.you07@gmail.com>
2018-05-28 21:44:11 -0400

    Create book_layout for the gridview

--- app/src/main/java/com/raywenderlich/babybookcase/BooksAdapter.java
+++ app/src/main/java/com/raywenderlich/babybookcase/BooksAdapter.java
@@ -1,12 +1,3 @@
-/*
- * PROJECT LICENSE:
- *
- * This project has been completed by Stephanie Youstra as part of the Udacity Android Developer Nanodegree.
- * Feel free to view this as a reference point, but remember Udacity's Honor Code requiring that submissions be original work. Any liability for the consequences of plagiarism lies solely with the "borrower".
- *
- * Copyright (c) 2018 ~ Stephanie Youstra.
- */
-
 package com.raywenderlich.babybookcase;
 
 import android.content.Context;
