f4f4889a1dd330033047e32a07f9676cac397550
Stephanie Youstra <steph.you07@gmail.com>
2018-05-29 01:33:54 -0400

    Create ViewHolder to speed up view loading

--- app/src/main/java/com/raywenderlich/babybookcase/BooksAdapter.java
+++ app/src/main/java/com/raywenderlich/babybookcase/BooksAdapter.java
@@ -40,25 +40,41 @@ public class BooksAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         final Book book = books[position];
 
+        //view holder pattern
         if (convertView == null) {
             final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
             convertView = layoutInflater.inflate(R.layout.linearlayout_book, null);
+
+            final ImageView imageViewCoverArt = (ImageView)convertView.findViewById(R.id.imageview_cover_art);
+            final TextView nameTextView = (TextView)convertView.findViewById(R.id.textview_book_name);
+            final TextView authorTextView = (TextView)convertView.findViewById(R.id.textview_book_author);
+            final ImageView imageViewFavorite = (ImageView)convertView.findViewById(R.id.imageview_favorite);
+
+            final ViewHolder viewHolder = new ViewHolder(nameTextView, authorTextView, imageViewCoverArt, imageViewFavorite);
+            convertView.setTag(viewHolder);
         }
 
-        final ImageView imageView = (ImageView)convertView.findViewById(R.id.imageview_cover_art);
-        final TextView nameTextView = (TextView)convertView.findViewById(R.id.textview_book_name);
-        final TextView authorTextView = (TextView)convertView.findViewById(R.id.textview_book_author);
-        final ImageView imageViewFavorite = (ImageView)convertView.findViewById(R.id.imageview_favorite);
-
-        imageView.setImageResource(book.getImageResource());
-        nameTextView.setText(mContext.getString(book.getName()));
-        authorTextView.setText(mContext.getString(book.getAuthor()));
-        imageViewFavorite.setImageResource(
-                book.getIsFavorite() ? R.drawable.star_enabled : R.drawable.star_disabled
-        );
-
+        final ViewHolder viewHolder = (ViewHolder)convertView.getTag();
+        viewHolder.imageViewCoverArt.setImageResource(book.getImageResource());
+        viewHolder.nameTextView.setText(mContext.getString(book.getName()));
+        viewHolder.authorTextView.setText(mContext.getString(book.getAuthor()));
+        viewHolder.imageViewFavorite.setImageResource(book.getIsFavorite() ? R.drawable.star_enabled : R.drawable.star_disabled);
+        
         return convertView;
     }
 
+    private class ViewHolder {
+        private final TextView nameTextView;
+        private final TextView authorTextView;
+        private final ImageView imageViewCoverArt;
+        private final ImageView imageViewFavorite;
+
+        public ViewHolder (TextView nameTextView, TextView authorTextView, ImageView imageViewCoverArt, ImageView imageViewFavorite) {
+            this.nameTextView = nameTextView;
+            this.authorTextView = authorTextView;
+            this.imageViewCoverArt = imageViewCoverArt;
+            this.imageViewFavorite = imageViewFavorite;
+        }
+    }
 
 }
